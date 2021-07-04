ed106d2487e28df1880c42a3b4d8e2716704becb
Petya Todorova <invisioncl@gmail.com>
2021-07-04 20:11:03 +0300

    Server is listening.

---
+++ WebFrameworkSoftuni/WebFrameworkSoftuni/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace WebFrameworkSoftuni
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var address = IPAddress.Parse("127.0.0.1");
+            var port = 8080;
+
+            var serverListener = new TcpListener(address, port);
+
+            serverListener.Start();
+
+            var connection = await serverListener.AcceptTcpClientAsync();
+
+            Console.WriteLine($"Server started at port {port}");
+            Console.WriteLine("Listening for requests.");
+        }
+    }
+}
